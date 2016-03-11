8401c5de77af8c6a1f39d28631ff6619dce8acf5
root <ngohuynhngockhanh@gmail.com>
2016-03-11 17:12:10 +0000

    init server nodejs and add phpjs

---
+++ nodejs/server.js
@@ -0,0 +1,29 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(1234);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.on('connection', function (socket) {
+	socket.emit('play', {url: 'http://www.ailab.hcmus.edu.vn/voice_adaption/uploads/ngan.mp3'});
+	setTimeout(function() {
+		socket.emit('play', {url: 'http://www.ailab.hcmus.edu.vn/voice_adaption/uploads/tb.mp3'});
+	}, 1000);
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
